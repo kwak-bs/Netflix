ca9f9a7d76608e82489ce0e7b8773322ab3dfade
kwak-bs <iam-bs@naver.com>
2025-05-19 02:12:08 +0900

    feat : jwt strategy passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entity/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './strategy/local.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([
@@ -15,7 +16,7 @@ import { LocalStrategy } from './strategy/local.strategy';
   })
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService] // AuthModule을 import했을때 AuthService도 같이 inject받을 수 있음.
 })
 export class AuthModule { }
